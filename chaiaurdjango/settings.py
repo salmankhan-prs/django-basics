cb035441d4a02bc59e69b6bc250f1c585fadba45
PATHAN SALMAN KHAN <54095769+salmankhan-prs@users.noreply.github.com>
2024-07-10 19:17:50 +0530

    feat: basic version 2

--- chaiaurdjango/settings.py
+++ chaiaurdjango/settings.py
@@ -128,9 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') ##FIXME:
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
